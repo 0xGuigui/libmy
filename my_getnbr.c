abfd9b8676fea922217d549e2f7e9eada8bb3ba4
Guilhem Jéhanno <72016897+guilhem-jehanno@users.noreply.github.com>
2020-12-09 17:52:35 +0100

    [+] - Ajout d'un my_getnbr.c fonctionnel

---
+++ my_getnbr.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2020
+** MyLib
+** File description:
+** my_getnbr.c
+*/
+
+int my_getnbr(char const *str)
+{
+    int x = 1;
+    int y = 0;
+    int z = 0;
+
+    while (str[y] == 43 || str[y] == 45) {
+        if (str[y] == 45)
+            x = x * -1;
+        y = y + 1;
+    }
+    while (str[y] != '\0') {
+        if (str[y] >= 48 && str[y] <= 57) {
+            z = z * 10;
+            z = z + str[y] - 48;
+            y = y + 1;
+        }
+        else
+            return z * x;
+    }
+    return z * x;
+}
